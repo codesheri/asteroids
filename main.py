22a9e66b72c01ae02a20c96020a49bd8cd72fbea
Sheri Richardson <shericodes@protonmail.com>
2025-05-21 22:49:26 +0200

    refactor main function

--- main.py
+++ main.py
@@ -4,32 +4,27 @@ from player import Player
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-
+    # pygame setup
     pygame.init()
-
-    clock = pygame.time.Clock()
-    dt = 0
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    game_running = True
 
+    # groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
-    Player(x, y)
 
-    game_running = True
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while game_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
 
         updatable.update(dt)
 
